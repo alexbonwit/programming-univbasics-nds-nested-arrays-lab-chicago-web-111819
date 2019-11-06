6ed9f4bd36235459c552f05dab67dd1b9b9c61c8
Alex Bonwit <alexbonwit@mac.com>
2019-11-06 23:53:48 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -27,15 +27,27 @@ def array_literal_matrix
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
   residents = [
-    ].sort
+    ["Don Gately",
+  "Joelle van Dyne",
+  "Pat M.",
+  "Kate Gompert",
+  "Bruce Green"],
+    ["Hal Incandenza",
+  "Lyle",
+  "Gerhard Schtitt",
+  "Mario Incandenza",
+  "Michael Pemulis"]
+  ].sort
 end
 
 def matrix_lookup(matrix, row, column)
   # Return the matrix's content at row and column
-  residents = [ENNET_HOUSE, ENFIELD_TENNIS_ACADEMY]
+  matrix[row][column]
 end
 
 def matrix_update(matrix, row, column, new_value)
   # Update the matrix location at row and column to have the value of new_value
+  matrix[row][column] = new_value
   # Return the updated matrix
+  matrix
 end
